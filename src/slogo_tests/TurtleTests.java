7a19993d3a59b0fbbfabbf9311527140318182a7
Jeff Hou <j.king.hou@gmail.com>
2014-02-21 23:17:08 -0500

    Added tests.

---
+++ src/slogo_tests/TurtleTests.java
@@ -0,0 +1,56 @@
+package slogo_tests;
+
+import slogo_team12.Tuple;
+import slogo_team12.Turtle;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+public class TurtleTests {
+	@Test
+	public void turtleCreationTest() {
+		Turtle turtle = new Turtle();
+		Tuple position = turtle.getPosition();
+		assertEquals("Incorrect initial position", position.x, 0, 0.001);
+		assertEquals("Incorrect initial position", position.y, 0, 0.001);
+		assertEquals("Incorrect initial direction", turtle.getHeading(), 0, 0.001);
+		assertEquals("Incorrect pen state", turtle.getPenState(), true);
+		assertEquals("Incorrect turtle visibility", turtle.getVisibility(), true);
+		assertEquals("Incorrect number of paths", turtle.getPaths().size(), 1);
+		assertEquals("Incorrect number of nodes in initial path", turtle.getPaths().get(0).size(), 1);
+		assertEquals("Incorrect initial node in initial path", turtle.getPaths().get(0).get(0), new Tuple());
+	}
+	public void turtleSimpleMovementTest(){
+		Turtle turtle = new Turtle();
+		turtle.move(new Tuple(0, 5));
+		Tuple position = turtle.getPosition();
+		assertEquals("Incorrect position after position", position.x, 0, 0.001);
+		assertEquals("Incorrect position after position", position.y, 5, 0.001);
+		
+		turtle.move(new Tuple(-3.4, 0));
+		position = turtle.getPosition();
+		assertEquals("Incorrect position after position", position.x, -3.4, 0.001);
+		assertEquals("Incorrect position after position", position.y, 5, 0.001);
+
+		turtle.move(new Tuple(0, -2.209));
+		position = turtle.getPosition();
+		assertEquals("Incorrect position after position", position.x, 3.4, 0.001);
+		assertEquals("Incorrect position after position", position.y, 2.791, 0.001);
+
+		turtle.move(new Tuple(-1.1, 0));
+		position = turtle.getPosition();
+		assertEquals("Incorrect position after position", position.x, 4.5, 0.001);
+		assertEquals("Incorrect position after position", position.y, 2.791, 0.001);
+		
+		turtle.setHeading(163.0);
+		assertEquals("Incorrect heading after rotation", turtle.getHeading(), 163, 0.001);
+		
+		turtle.setHeading(-26);
+		assertEquals("Incorrect heading after rotation", turtle.getHeading(), 334, 0.001);
+	}
+}
