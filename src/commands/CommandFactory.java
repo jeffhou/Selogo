ba4cd982d9b5494b30f4dfe4311625371150c079
Jeff Hou <j.king.hou@gmail.com>
2014-02-25 00:51:14 -0500

    Added XML Reader for parsing purposes.

--- src/commands/CommandFactory.java
+++ src/commands/CommandFactory.java
@@ -2,6 +2,7 @@ package commands;
 
 import java.util.HashMap;
 
+import parser.XMLReader;
 import backend.Engine;
 import backend.Interpreter;
 import exceptions.InvalidCommandStringException;
@@ -16,41 +17,45 @@ public class CommandFactory {
 	Interpreter interpreter;
 	Engine engine;
 	public HashMap<String, String> commands = new HashMap<String, String>();
-
+	public void populateCommands(){
+		XMLReader newXMLReader = new XMLReader();
+		newXMLReader.read("assets/turtleCommands.xml", commands);
+	}
 	public CommandFactory(Interpreter interpreter, Engine engine) {
 		this.interpreter = interpreter;
 		this.engine = engine;
 		/**
-		 * TODO: this is duplicate code. Create a properties file or an XML file
+		 * TODO: this is duplicate code. Create a resource file or an XML file
 		 * and make the factory read it instead.
 		 */
-		commands.put("fd", "commands.turtleCommands.ForwardTurtleCommand");
-		commands.put("forward", "commands.turtleCommands.ForwardTurtleCommand");
-		commands.put("bk", "commands.turtleCommands.BackTurtleCommand");
-		commands.put("back", "commands.turtleCommands.BackTurtleCommand");
-		commands.put("lt", "commands.turtleCommands.LeftTurtleCommand");
-		commands.put("left", "commands.turtleCommands.LeftTurtleCommand");
-		commands.put("rt", "commands.turtleCommands.RightTurtleCommand");
-		commands.put("right", "commands.turtleCommands.RightTurtleCommand");
-		commands.put("seth", "commands.turtleCommands.SetHeadingTurtleCommand");
-		commands.put("setheading",
-				"commands.turtleCommands.SetHeadingTurtleCommand");
-		commands.put("goto", "commands.turtleCommands.GotoTurtleCommand");
-		commands.put("setxy", "commands.turtleCommands.GotoTurtleCommand");
-		commands.put("pd", "commands.turtleCommands.PenDownTurtleCommand");
-		commands.put("pendown", "commands.turtleCommands.PenDownTurtleCommand");
-		commands.put("pu", "commands.turtleCommands.PenUpTurtleCommand");
-		commands.put("penup", "commands.turtleCommands.PenUpTurtleCommand");
-		commands.put("st", "commands.turtleCommands.ShowTurtleTurtleCommand");
-		commands.put("showturtle",
-				"commands.turtleCommands.ShowTurtleTurtleCommand");
-		commands.put("ht", "commands.turtleCommands.HideTurtleTurtleCommand");
-		commands.put("hideturtle",
-				"commands.turtleCommands.HideTurtleTurtleCommand");
-		commands.put("home", "commands.turtleCommands.HomeTurtleCommand");
-		commands.put("cs", "commands.turtleCommands.ClearScreenTurtleCommand");
-		commands.put("clearscreen",
-				"commands.turtleCommands.ClearScreenTurtleCommand");
+		populateCommands();
+//		commands.put("fd", "commands.turtleCommands.ForwardTurtleCommand");
+//		commands.put("forward", "commands.turtleCommands.ForwardTurtleCommand");
+//		commands.put("bk", "commands.turtleCommands.BackTurtleCommand");
+//		commands.put("back", "commands.turtleCommands.BackTurtleCommand");
+//		commands.put("lt", "commands.turtleCommands.LeftTurtleCommand");
+//		commands.put("left", "commands.turtleCommands.LeftTurtleCommand");
+//		commands.put("rt", "commands.turtleCommands.RightTurtleCommand");
+//		commands.put("right", "commands.turtleCommands.RightTurtleCommand");
+//		commands.put("seth", "commands.turtleCommands.SetHeadingTurtleCommand");
+//		commands.put("setheading",
+//				"commands.turtleCommands.SetHeadingTurtleCommand");
+//		commands.put("goto", "commands.turtleCommands.GotoTurtleCommand");
+//		commands.put("setxy", "commands.turtleCommands.GotoTurtleCommand");
+//		commands.put("pd", "commands.turtleCommands.PenDownTurtleCommand");
+//		commands.put("pendown", "commands.turtleCommands.PenDownTurtleCommand");
+//		commands.put("pu", "commands.turtleCommands.PenUpTurtleCommand");
+//		commands.put("penup", "commands.turtleCommands.PenUpTurtleCommand");
+//		commands.put("st", "commands.turtleCommands.ShowTurtleTurtleCommand");
+//		commands.put("showturtle",
+//				"commands.turtleCommands.ShowTurtleTurtleCommand");
+//		commands.put("ht", "commands.turtleCommands.HideTurtleTurtleCommand");
+//		commands.put("hideturtle",
+//				"commands.turtleCommands.HideTurtleTurtleCommand");
+//		commands.put("home", "commands.turtleCommands.HomeTurtleCommand");
+//		commands.put("cs", "commands.turtleCommands.ClearScreenTurtleCommand");
+//		commands.put("clearscreen",
+//				"commands.turtleCommands.ClearScreenTurtleCommand");
 	}
 
 	public Command createCommand(String firstWord)
---
+++ src/parser/XMLReader.java
@@ -0,0 +1,45 @@
+package parser;
+
+import java.io.File;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class XMLReader {
+
+	public XMLReader() {
+	}
+
+	protected Document makeDocumentFromFile(String fileName) {
+		try {
+			File fXmlFile = new File(fileName);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
+			doc.getDocumentElement().normalize();
+			return doc;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public void read(String filename, Map<String, String> commands) {
+		Document doc = this.makeDocumentFromFile(filename);
+		NodeList nList = doc.getElementsByTagName("command");
+		for (int i = 0; i < nList.getLength(); i++) {
+			Node nNode = nList.item(i);
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+				commands.put(eElement.getAttribute("code"), eElement.getAttribute("commandClass"));
+			}
+		}
+	}
+
+}
