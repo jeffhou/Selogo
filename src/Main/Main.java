068c34fd78f4ce0448e657e972875b7f958d70f1
Cody Lieu <mikegillett@wl-10-190-4-218.wireless.duke.edu>
2014-02-27 11:33:31 -0500

    More Refactoring of the Main Class

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -53,6 +53,28 @@ public class Main {
 	private static InterpreterGUI newInterpreterGUI;
 	public static void main(String[] args) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException{
 		
-		
+		newInterpreterGUI = new InterpreterGUI(new Interpreter());
+		// Schedule a job for the event dispatch thread:
+		// creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					newInterpreterGUI.createAndShowGUI();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InstantiationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+
 	}
 }
