bf166b2852c28caf4d3d8b55365956d00c6590dc
Jeff Hou <j.king.hou@gmail.com>
2014-03-02 13:34:44 -0500

    Fixed a typo

--- src/gui/menubar/TurtleMenu.java
+++ src/gui/menubar/TurtleMenu.java
@@ -5,7 +5,7 @@ import javax.swing.JMenu;
 public class TurtleMenu extends JMenu{
 	public TurtleMenu(){
 		super("Turtle");
-		add(new HelpMenuItem());
+		add(new ImageMenuItem());
 		add(new PenColorMenuItem());
 		add(new StatsMenuItem());
 	}
