3f08bfc3d76b5cca1b42efe274ddf43432680544
dianwen <dianwen@gmail.com>
2014-03-04 22:56:56 -0500

    Added user defined commands

---
+++ src/backend/UserCommand.java
@@ -0,0 +1,17 @@
+package backend;
+
+import java.util.Arrays;
+
+public class UserCommand {
+	public String[] parameters;
+	public String commands;
+	
+	public UserCommand(String parameterString, String commandString) {
+		commands = commandString;
+		parameters = parameterString.split(" ");
+	}
+	
+	public String toString() {
+		return "parameters: " + Arrays.toString(parameters) + "\n" + "commands: " + commands;
+	}
+}
