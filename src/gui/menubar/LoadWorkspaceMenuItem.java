8b4219b4c15956400fbe6ee89d18b48eed2354b9
Jeff Hou <j.king.hou@gmail.com>
2014-03-07 01:56:45 -0500

    Made Save/Load work for variables/commands.

--- src/gui/menubar/LoadWorkspaceMenuItem.java
+++ src/gui/menubar/LoadWorkspaceMenuItem.java
@@ -1,28 +1,28 @@
 package gui.menubar;
 
-import java.awt.Desktop;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.File;
 import java.io.IOException;
 
 import javax.swing.JMenuItem;
+
+import util.Serializer;
+import backend.WorldsCollection;
 
 
 public class LoadWorkspaceMenuItem extends JMenuItem {
 	LoadWorkspaceMenuItem(){
 		super("Load Workspace");
-//		addMouseListener(new LaunchNewTabMouseListener());
+		addMouseListener(new LaunchNewTabMouseListener());
 	}
-//	class LaunchNewTabMouseListener extends MouseAdapter{
-//		public void mousePressed(MouseEvent e) {
-//			File file = new File("src/help.html");
-//			Desktop desktop = Desktop.getDesktop();
-//			try {
-//				desktop.open(file);
-//			} catch (IOException e1) {
-//				e1.printStackTrace();
-//			}
-//		}
-//	}
+	class LaunchNewTabMouseListener extends MouseAdapter{
+		public void mousePressed(MouseEvent e) {
+			try {
+				WorldsCollection.getInstance().getCurrentWorld().setUserCommands(Serializer.deserialize("userCommands.ser"));
+				WorldsCollection.getInstance().getCurrentWorld().setVariables(Serializer.deserialize("userVariables.ser"));
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
 }
--- src/gui/menubar/SaveWorkspaceMenuItem.java
+++ src/gui/menubar/SaveWorkspaceMenuItem.java
@@ -1,28 +1,27 @@
 package gui.menubar;
 
-import java.awt.Desktop;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.File;
 import java.io.IOException;
-
 import javax.swing.JMenuItem;
+
+import util.Serializer;
+import backend.WorldsCollection;
 
 
 public class SaveWorkspaceMenuItem extends JMenuItem {
 	SaveWorkspaceMenuItem(){
 		super("Save Workspace");
-//		addMouseListener(new LaunchNewTabMouseListener());
+		addMouseListener(new LaunchNewTabMouseListener());
 	}
-//	class LaunchNewTabMouseListener extends MouseAdapter{
-//		public void mousePressed(MouseEvent e) {
-//			File file = new File("src/help.html");
-//			Desktop desktop = Desktop.getDesktop();
-//			try {
-//				desktop.open(file);
-//			} catch (IOException e1) {
-//				e1.printStackTrace();
-//			}
-//		}
-//	}
+	class LaunchNewTabMouseListener extends MouseAdapter{
+		public void mousePressed(MouseEvent e) {
+			try {
+				Serializer.serialize("userCommands.ser", WorldsCollection.getInstance().getCurrentWorld().getUserCommands());
+				Serializer.serialize("userVariables.ser", WorldsCollection.getInstance().getCurrentWorld().getVariables());
+			} catch(IOException i) {
+				i.printStackTrace();
+			}
+		}
+	}
 }
